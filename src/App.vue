dffc0851ec8ece593af6a59a9e210841ffd9446c
jacopo-derrico <derricoj.design@gmail.com>
2024-02-06 15:00:44 +0100

    created component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
-<script setup>
+<script>
+  import AppTitle from './components/AppTitle.vue' 
 
+  export default {
+        components: {
+          AppTitle
+        }
+    }
 </script>
 
 <template>
-  
+  <AppTitle/>
 </template>
 
 <style>
-
+ #app {
+  display: flex;
+ }
 
 @media (min-width: 1024px) {
   
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: "AppTitle"
+}
+</script>
+
+<template>
+    <h1>La mia prima app con Vite!</h1>
+</template>
+
+<style scoped>
+h1 {
+    font-size: 50px;
+    color: white;
+    margin: 0 auto;
+}
+
+@media (min-width: 1024px) {}
+</style>
